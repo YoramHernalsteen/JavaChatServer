047e552bd804f600b1ddae01cdeaf54b94af6e94
YoramHernalsteen <r0582769@student.thomasmore.be>
2021-02-19 22:04:20 +0100

    start of JavaChat

---
+++ src/test/java/com/javachat/websocket/JavaChatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javachat.websocket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaChatApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
