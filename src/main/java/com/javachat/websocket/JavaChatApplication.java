bd7262f8c1246b488533d03f05915e5b6feea63d
YoramHernalsteen <r0582769@student.thomasmore.be>
2021-02-19 22:36:25 +0100

    cors checking

--- src/main/java/com/javachat/websocket/JavaChatApplication.java
+++ src/main/java/com/javachat/websocket/JavaChatApplication.java
@@ -2,12 +2,24 @@ package com.javachat.websocket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class JavaChatApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JavaChatApplication.class, args);
+    }
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**").allowCredentials(true).allowedOrigins("*").allowedMethods("*");
+            }
+        };
     }
 
 }
