27dc382c86ac6f6580d855a2a5e443fea9fb3207
YoramHernalsteen <r0582769@student.thomasmore.be>
2021-02-20 17:11:10 +0100

    enabling course for all origins

--- src/main/java/com/javachat/websocket/config/WebSocketConfig.java
+++ src/main/java/com/javachat/websocket/config/WebSocketConfig.java
@@ -15,7 +15,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
     @Override
     public void registerStompEndpoints(StompEndpointRegistry stompEndpointRegistry) {
         stompEndpointRegistry.addEndpoint("/websocket-chat")
-                .setAllowedOrigins(urlClient)
+                .setAllowedOrigins("*")
                 .withSockJS();
     }
     @Override
--- src/main/java/com/javachat/websocket/controllers/WebSocketController.java
+++ src/main/java/com/javachat/websocket/controllers/WebSocketController.java
@@ -5,7 +5,9 @@ import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin(origins = "*")
 @Controller
 public class WebSocketController {
 
