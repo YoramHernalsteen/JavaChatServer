b86a5ad0acf9961430db866f33bb298062267263
YoramHernalsteen <r0582769@student.thomasmore.be>
2021-02-20 17:41:48 +0100

    preflight request ok, info not found

--- src/main/java/com/javachat/websocket/config/WebSocketConfig.java
+++ src/main/java/com/javachat/websocket/config/WebSocketConfig.java
@@ -3,10 +3,12 @@ package com.javachat.websocket.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
 import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
 import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
 
+@Controller
 @Configuration
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
--- src/main/java/com/javachat/websocket/controllers/WebSocketController.java
+++ src/main/java/com/javachat/websocket/controllers/WebSocketController.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class WebSocketController {
 
     @MessageMapping("/user-all")
